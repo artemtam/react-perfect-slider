3c005821b97cc73ae4869cab746a12dae6334f8b
Artem Tamoian <artemtam1408@gmail.com>
2021-02-07 00:07:53 +0200

    chore: upgrade dependencies, clean up ESLint configuration

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,11 +1,10 @@
-/* eslint-disable */
 /// <reference types="node" />
 /// <reference types="react" />
 /// <reference types="react-dom" />
 
 declare namespace NodeJS {
-    interface ProcessEnv {
-        readonly NODE_ENV: 'development' | 'production' | 'test';
-        readonly PUBLIC_URL: string;
-    }
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+    readonly PUBLIC_URL: string;
+  }
 }
